4b481db0d822d4a832a89fdd3fad5d5f59d10f05
Vadim <malin.vadimka@mail.ru>
2023-03-27 23:43:41 +0300

    Fix ex

--- Kispython/16/ex10(16).py
+++ Kispython/16/ex10(16).py
@@ -1,4 +1,4 @@
-class MealyStateMachine:
+class StateMachine:
     def __init__(self):
         self.state = 'A'
 
@@ -16,7 +16,7 @@ class MealyStateMachine:
             case 'G':
                 self.state = 'C'
                 return 8
-        return MealyError("amass")
+        raise MealyError("amass")
 
     def view(self):
         match(self.state):
@@ -32,7 +32,7 @@ class MealyStateMachine:
             case 'G':
                 self.state = 'D'
                 return 9
-        return MealyError("view")
+        raise MealyError("view")
 
     def scale(self):
         match(self.state):
@@ -42,52 +42,54 @@ class MealyStateMachine:
             case 'F':
                 self.state = 'F'
                 return 7
-        return MealyError("scale")
+        raise MealyError("scale")
 
 
 class MealyError(Exception):
-    def __init__(self, method_name):
-        self.method_name = method_name
-
-    def __str__(self):
-        return "MealyError"
+    pass
 
 
 def main():
-    return MealyStateMachine()
+    return StateMachine()
+
+
+def raises(method, error):
+    output = None
+    try:
+        output = method()
+    except Exception as e:
+        assert type(e) == error
+    assert output is None
 
 
 def test():
-    o = MealyStateMachine()
-    me_a = MealyError("amass")
-    me_s = MealyError("scale")
-    me_v = MealyError("view")
+    o = main()
     assert o.state == 'A'
-    assert o.scale().method_name == me_s.method_name
-    assert o.view().method_name == me_v.method_name
+    raises(lambda: o.scale(), MealyError)
+    raises(lambda: o.view(), MealyError)
     assert o.amass() == 0
     assert o.state == 'B'
-    assert o.amass().method_name == me_a.method_name
-    assert o.scale().method_name == me_s.method_name
+    raises(lambda: o.amass(), MealyError)
+    raises(lambda: o.scale(), MealyError)
     assert o.view() == 1
     assert o.state == 'C'
-    assert o.scale().method_name == me_s.method_name
+    raises(lambda: o.scale(), MealyError)
     assert o.view() == 3
     assert o.state == 'F'
-    assert o.view().method_name == me_v.method_name
+    raises(lambda: o.view(), MealyError)
     assert o.scale() == 7
     assert o.state == 'F'
     assert o.amass() == 6
     assert o.state == 'G'
-    assert o.scale().method_name == me_s.method_name
+    raises(lambda: o.scale(), MealyError)
     assert o.view() == 9
     assert o.state == 'D'
-    assert o.amass().method_name == me_a.method_name
-    assert o.scale().method_name == me_s.method_name
+    raises(lambda: o.amass(), MealyError)
+    raises(lambda: o.scale(), MealyError)
     assert o.view() == 4
     assert o.state == 'E'
-    assert o.amass().method_name == me_a.method_name
-    assert o.view().method_name == me_v.method_name
+    raises(lambda: o.amass(), MealyError)
+    raises(lambda: o.view(), MealyError)
     assert o.scale() == 5
     assert o.state == 'F'
     assert o.amass() == 6
@@ -97,52 +99,9 @@ def test():
     assert o.amass() == 2
     assert o.state == 'D'
     o.state = 'X'
-    assert o.amass().method_name == me_a.method_name
-    assert o.view().method_name == me_v.method_name
-    assert o.scale().method_name == me_s.method_name
-
-
-def test_1():
-    o = main()
-    print(o.view()) # MealyError
-    print(o.amass()) # 0
-    print(o.view()) # 1
-    print(o.view()) # 3
-    print(o.view()) # MealyError
-    print(o.amass()) # 6
-    print(o.view()) # 9
-    print(o.scale()) # MealyError
-    print(o.view()) # 4
-    print(o.amass()) # MealyError
-    print(o.scale()) # 5
-    print(o.view()) # MealyError
-    print(o.amass()) # 6
-    print(o.amass()) # 8
-    print(o.amass()) # 2
-    print(o.view()) # 4
-    print(o.scale()) # 5
-    print(o.scale()) # 7
-    
-
-def test_2():
-    o = main()
-    print(o.amass()) # 0
-    print(o.view()) # 1
-    print(o.amass()) # 2
-    print(o.scale()) # MealyError
-    print(o.view()) # 4
-    print(o.scale()) # 5
-    print(o.scale()) # 7
-    print(o.amass()) # 6
-    print(o.scale()) # MealyError
-    print(o.amass()) # 8
-    print(o.view()) # 3
-    print(o.amass()) # 6
-    print(o.view()) # 9
-    print(o.view()) # 4
-    print(o.scale()) # 5
+    raises(lambda: o.amass(), MealyError)
+    raises(lambda: o.view(), MealyError)
+    raises(lambda: o.scale(), MealyError)
 
 
 test()
-# test_1()
-# test_2()
